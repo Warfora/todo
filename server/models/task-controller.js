150b40c395c707dd60d76d2d873b87ce064e7dd7
warfora <t3seka00@students.oamk.fi>
2025-09-08 10:26:17 +0300

    todo 8 (not ready)

---
+++ server/models/task-controller.js
@@ -0,0 +1,42 @@
+import { selectAllTasks } from '../models/task.js'
+import { ApiError } from '../helper/error.js'
+
+const getTasks = async (req, res,next) => {
+    try {
+        const result = await selectAllTasks()
+    return res.status(200).json(result.rows || [])
+    } catch (error) {
+        return next(error)
+    }
+};
+
+const postTask = async (req, res,next) => {
+    const { task } = req.body
+    try {
+        if (!task || !task.description || task.description.trim().length === 0) {
+            return next(new ApiError('Task description is required', 400))
+            // const error = new Error('Task description is required')
+            // error.status = 400
+            // return next(error)
+        }
+        const result = await insertTask(task.description)
+        return res.status(201).json({id: result.rows[0].id, description: result.rows[0].description})
+    } catch (error) {
+        return next(error)
+    }
+};
+
+const deleteTask = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const result = await deleteTaskById(id);
+        if (result.rowCount === 0) {
+            return next(new ApiError('Task not found', 404));
+        }
+        return res.status(200).json({ id });
+    } catch (error) {
+        return next(error);
+    }
+};
+
+export { getTasks, postTask, deleteTask };
--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -1,48 +1,11 @@
-import { pool } from '../helper/db.js'
 import { Router } from 'express'
+import { getTasks, postTask, deleteTask } from '../controllers/task-controller.js'
+
 
 const router = Router()
 
-router.get('/', (req, res, next) => {
-    pool.query('SELECT * FROM task', (err, result) => {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).json(result.rows);
-    });
-});
-
-router.post('/create', (req, res, next) => {
-    const { task } = req.body;
-
-    if (!task) {
-        return res.status(400).json({ error: 'Task is required' });
-    }
-
-    pool.query(
-        'insert into task (description) values ($1) returning *',
-        [task.description],
-        (err, result) => {
-            if (err) {
-                return next(err);
-            }
-            res.status(201).json({ id: result.rows[0].id, description: task.description });
-        }
-    );
-});
-
-router.delete('/delete/:id', (req, res, next) => {
-    const { id } = req.params;
-    console.log(`Deleting task with id: ${id}`);
-    pool.query('delete from task WHERE id = $1', [id], (err, result) => {
-        if (err) {
-            return next(err);
-        }
-        if (result.rowCount === 0) {
-            return res.status(404).json({ error: 'Task not found' });
-        }
-        return res.status(200).json({ id: id });
-    });
-});
+router.get('/', getTasks);
+router.post('/create', postTask);
+router.delete('/delete/:id', deleteTask);
 
 export default router;
